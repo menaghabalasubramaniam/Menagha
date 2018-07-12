ad14127845bce76d8743072110048711fc22d7f3
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-12 10:25:40 +0530

    Create postive.java

---
+++ postive.java
@@ -0,0 +1,22 @@
+
+public class Postive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				int n=345;
+				if(n>0){
+					System.out.println("the no is postive");
+				}
+				else if(n<0){
+					System.out.println("the no is negative");
+				}
+				else{
+					System.out.println("zero");
+				}
+
+			}
+}
+
+		
+	
