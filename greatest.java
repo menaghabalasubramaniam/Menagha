e4e04cc5a5a844dcdd25fe2c8109eb27c9b541a8
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-13 17:05:16 +0530

    Create greatest.java

---
+++ greatest.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=1;
+		int b=2;
+		int c=3;
+		if(a>b && a>c){
+		System.out.println("a is greatest");
+	}
+	else if(b>a && b>c){
+		System.out.println("b is greatest");
+	}
+	else{
+		System.out.println("c is greatest");
+	}
+	}
+}
