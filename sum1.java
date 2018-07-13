b56d5bcfdfbd85661aaca6776161f5cf14540182
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-13 17:46:01 +0530

    Create sum1.java

---
+++ sum1.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=5;
+		int b=3;
+		int i,sum=0;
+		int arr[]={1,2,3,4,5};
+		for(i=0;i<b;i++){
+		sum=sum+arr[i];
+		}
+		System.out.println(sum);
+	}
+}
