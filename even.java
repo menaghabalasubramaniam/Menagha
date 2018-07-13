e1722462baa4ee6d4f52d377b7bc9e706e745f7e
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-13 11:11:19 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n=20;
+		if(n%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+	}
+}
