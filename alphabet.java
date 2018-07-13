87cd54af674197edcbaba0d184dfc4e956e4d630
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-13 16:59:34 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch='a';
+		if(ch=='a' && ch=='z' || ch=='A' && ch=='Z')
+		System.out.println("alphabet");
+		else
+		System.out.print("not alphabet");
+	}
+}
