e23e2cc6397138fb38ed51aa21e8000659e967c6
menaghabalasubramaniam <41136699+menaghabalasubramaniam@users.noreply.github.com>
2018-07-29 10:28:38 +0530

    Create divide.java

---
+++ divide.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class GuviDivideBy2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+if(n%2==0){
+	System.out.println("0");
+}
+else{
+	System.out.println("1");
+}
+	}
+
+}
